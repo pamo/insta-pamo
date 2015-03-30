debe4a02896f8169aad12acf4f8f01ab75c105c8
Pamela Ocampo <pamela.ocampo@gmail.com>
2015-03-30 21:44:56 +0200

    More details on implementation

--- controller.js
+++ controller.js
@@ -1,65 +1,59 @@
 var Todo        = require('./todo.js').Todo;
 
 var addUrl = function(server, todo){
-   var baseUrl = server.protocol + '://' + server.address + ':' + server.port;
+   var baseUrl = 'http://' + server.host;
    todo.url = baseUrl + '/' + todo._id;
    return todo;
 }
 
 var save = function(request, reply){
-    todo = new Todo();
-    todo.title = request.payload.title;
-    todo.order = request.payload.order;
-    todo.url = request.info.remoteAddress;
+   todo = new Todo();
+   todo.title = request.payload.title;
+   todo.order = request.payload.order;
 
-    todo.save(function (err) {
-	if (!err) {
-	    var serverInfo = request.server.info;
-	    var response = addUrl(serverInfo, todo);
-	    reply(response);
-	} else {
-	    reply(Hapi.error.internal('Internal MongoDB error', err));
-	}
-    });
+   todo.save(function (err) {
+    if (!err) {
+      var response = addUrl(request.info, todo);
+      reply(response);
+    } else {
+      reply(Hapi.error.internal('Internal MongoDB error', err));
+    }
+   });
 };
 
 var update = function(request, reply){
    Todo.findOneAndUpdate(request.params.id, request.payload, function (err, todo) {
-	if (!err) {
-	   var serverInfo = request.server.info;
-	   var response = addUrl(serverInfo, todo);
-	   reply(response);
-	} else {
-	   reply(Hapi.error.internal('Internal MongoDB error', err));
-	}
-    });
-
+    if (!err) {
+      var response = addUrl(request.info, todo);
+      reply(response);
+    } else {
+      reply(Hapi.error.internal('Internal MongoDB error', err));
+    }
+  });
 };
 
 var getAll = function(request, reply){
    var todosWithUrl = [];
-   var serverInfo = request.server.info;
-    Todo.find({}, function (err, todos) {
-	if (!err) {
-	   for(i in todos){
-	      todosWithUrl.push(addUrl(serverInfo, todos[i]));
-	   }
-	   reply(todosWithUrl);
-	} else {
-	    reply(err);
-	}
-    });
+  Todo.find({}, function (err, todos) {
+      if (!err) {
+	       for(i in todos){
+		 todosWithUrl.push(addUrl(request.info, todos[i]));
+	       }
+	 reply(todosWithUrl);
+      } else {
+	reply(err);
+      }
+   });
 };
 
 var getById = function(request, reply){
-    var serverInfo = request.server.info;
-    Todo.findById(request.params.id, function(err, todo){
-	if (err){
-	    reply(err);
-	}
-	var response = addUrl(serverInfo, todo);
-	reply(response);
-    });
+   Todo.findById(request.params.id, function(err, todo){
+      if (err){
+	       reply(err);
+      }
+      var response = addUrl(request.info, todo);
+      reply(response);
+   });
 };
 
 var deleteAll = function(request, reply) {
@@ -71,9 +65,9 @@ var deleteAll = function(request, reply) {
 
 var deleteById = function(request, reply) {
     Todo.findById(request.params.id, function (err, todo){
-	if (err) return reply(Hapi.error.internal('Internal MongoDB error', err));
-	todo.remove();
-	reply("Record Deleted");
+        if (err) return reply(Hapi.error.internal('Internal MongoDB error', err));
+        todo.remove();
+        reply("Record Deleted");
     });
 };
 
