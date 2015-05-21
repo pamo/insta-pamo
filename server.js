d8d31200178004e912eea5d4410be7dff74ba5f0
Pamela Ocampo <pamela.ocampo@gmail.com>
2015-05-21 12:34:36 +0200

    Get the service to respond with tagged images

--- server.js
+++ server.js
@@ -1,36 +1,55 @@
-var config    = require('./config'),
-    Hapi      = require('hapi'),
-    server    = new Hapi.Server(3000, { cors: true }),
-    instagram = require('instagram')
-                .createClient(config.instagram.keys.id, config.instagram.keys.secret),
-    twitter   = require('twitter'),
-    twit      = new twitter(config.twitter);
+var config      = require('config'),
+    _           = require('lodash'),
+    instaConfig = config.get('Instagram'),
+    Hapi        = require('hapi'),
+    server      = new Hapi.Server(5000, { cors: true });
 
-var nameDrop = function (request, reply) {
-  reply('<a href="http://jrpz.github.io">jrpz</a>');
+
+function Photo(data){
+      return {
+        coordinates: [ data.location.longitude, data.location.latitude ],
+        title: data.location.name,
+        id: data.location.id, 
+        imageSrc: data.images.standard_resolution, 
+        url: data.link, 
+        description: data.caption.text
+      }
 }
 
-var getInstagramFeed = function (request, reply) {
-  // Grab Instagram Feed
-  instagram.users.media(config.instagram.user.id, { count: 15 }, 
-    function (images, err, pagination) {
-      return reply(images);
-    });
-};
+var nameDrop = function (request, reply) {
+  reply('Pamela Ocampo <a href="http://likescoffee.com">likes coffee.</a>');
+}
 
-var getTwitterFeed = function (request, reply) {
-  twit.get('/statuses/user_timeline.json', { screen_name: 'jrpz' }, 
-  function (tweets, res) {
-    return reply(tweets);
-  });
-};
+var getInstagramFeed = function (request, reply) { 
+  instagram = require('instagram-node').instagram();
+  instagram.use(instaConfig.client);
+  var images = [];
+
+  var getNext = function(err, medias, pagination, remaining, limit){
+    if(pagination.next){
+      for (var i = 0; i < medias.length; i++){
+        if(medias[i]['tags'].indexOf(request.params.tag) > -1){
+          photo = new Photo(medias[i]);
+          images.push(photo);
+        }
+      }
+      pagination.next(getNext);
+    }
+    else {
+      reply({ 
+        photos: _.flatten(images)
+      });
+    }
+  }
+
+  instagram.user_media_recent(instaConfig.user_id, getNext);
+}
 
 // Routes
+server.route({ method: 'GET', path: '/instagram/{tag}', handler: getInstagramFeed });
 server.route({ method: 'GET', path: '/', handler: nameDrop });
-server.route({ method: 'GET', path: '/instagram', handler: getInstagramFeed });
-server.route({ method: 'GET', path: '/twitter', handler: getTwitterFeed });
 
 // Start Server
 server.start(function () {
-  console.log('Server runnint at: ', server.info.uri);
+  console.log('Server running at: ', server.info.uri);
 });
