1acd6bf050ce7b642ca489866e2e0a1596927c95
Pamela Ocampo <pamela.ocampo@gmail.com>
2015-12-06 08:30:23 -0800

    Adds view templating engine

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 var Hapi        = require('hapi');
+var Path        = require('path');
+var Hoek        = require('hoek');
+var routes      = require('./routes.js');
 var mongoConfig      = require('./config.js').mongo;
 var Mongoose    = require('mongoose');
 var server      = new Hapi.Server();
 
 Mongoose.connect(mongoConfig.url);
-
 server.connection({
     port: parseInt(process.env.PORT) || 3000,
     routes: {cors: true}
 });
 
-var plugins = [ { register: require('./routes.js') }];
+server.views({
+    engines: {
+        html: require('handlebars')
+    },
+    path: 'views'
+});
 
-server.register(plugins, function (err) {
-    if (err) { throw err; }
-    server.start(function () {
-       console.log('Server running at:', server.info.uri);
-    });
+server.route(routes);
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
 });
--- routes.js
+++ routes.js
@@ -1,35 +1,20 @@
 var controller = require('./controller.js');
 var instagramController = require('./instagramController.js');
 
-exports.register = function(server, options, next) {
-  server.route({
+module.exports = [{
     method: 'GET',
     path: '/',
     handler: controller.getAll
-  });
-
-  server.route({
+}, {
     method: 'GET',
     path: '/{id}',
     handler: controller.getById
-  });
-
-  server.route({
+}, {
     method: 'GET',
     path: '/save/cafefront',
     handler: instagramController.saveCafeFronts
-  });
-
-  server.route({
+}, {
     method: 'GET',
     path: '/tag/{tag}',
     handler: instagramController.fetchTagged
-  });
-
-  next();
-};
-
-exports.register.attributes = {
-    name: 'routes',
-    version: '2.0.0'
-};
+}];
