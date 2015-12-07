3fd04978fd7ce05b97c173e708487959c448ffce
Pamela Ocampo <pamela.ocampo@gmail.com>
2015-12-06 22:42:30 -0800

    Updates to how to get and render photo schema

--- controller.js
+++ controller.js
@@ -2,32 +2,36 @@ var Photo = require('./photo.js').Photo;
 
 var getAll = function(request, reply) {
     Photo.find({}, function(err, photos) {
-	if (!err) {
-	    reply(photos);
-	} else {
-	    reply(err);
-	}
+        if (!err) {
+            reply(photos);
+        } else {
+            reply(err);
+        }
     }).sort({
-	created_time: -1
+        created_time: -1
     });
 }
 var renderAll = function(request, reply) {
     Photo.find({}, function(err, photos) {
-	if (!err) {
-	    reply.view('index', { photos: photos });
-	} else {
-	    reply(err);
-	}
-    }).sort({ created_time: -1 });
+        if (!err) {
+            reply.view('index', {
+                photos: JSON.parse(JSON.stringify(photos))
+            });
+        } else {
+            reply(err);
+        }
+    }).sort({
+        created_time: -1
+    });
 }
 
 var getById = function(request, reply) {
     Photo.findById(request.params.id, function(err, photo) {
-	if (!err) {
-	    reply(photo);
-	} else {
-	    reply(err);
-	}
+        if (!err) {
+            reply(photo);
+        } else {
+            reply(err);
+        }
     });
 };
 
--- photo.js
+++ photo.js
@@ -3,58 +3,68 @@ var Schema = Mongoose.Schema;
 var _ = require('lodash');
 
 var schema = new Schema({
-  tags: [],
-  location: {
-    latitude: Number,
-    name: String,
-    longitude: Number,
-    id: Number
-  },
-  created_time: Number,
-  link: String,
-  likes: {
-    count: Number
-  },
-  images: {
-    low_resolution: {
-      url: String
+    tags: [],
+    location: {
+        latitude: Number,
+        name: String,
+        longitude: Number,
+        id: Number
     },
-    standard_resolution: {
-      url: String
+    created_time: Number,
+    link: String,
+    likes: {
+        count: Number
     },
-    thumbnail: {
-      url: String
+    images: {
+        low_resolution: {
+            url: String
+        },
+        standard_resolution: {
+            url: String
+        },
+        thumbnail: {
+            url: String
+        }
+    },
+    description: String,
+    caption: {
+        text: String
+    },
+    id: {
+        type: String,
+        unique: true
     }
-  },
-  description: String,
-  caption: {
-    text: String
-  },
-  id: {
-    type: String,
-    unique: true
-  }
 });
 
 
-var transformDescription = function(description){
-  return _.dropRight(_.drop(description.slice(13, description.length).split('.')));
+var transformDescription = function(description) {
+    return _.dropRight(_.drop(description.slice(13, description.length).split('.')));
+}
+
+var transformObject = function(ret) {
+    return {
+        created_time: ret.created_time,
+        coordinates: [ret.location.longitude, ret.location.latitude],
+        title: ret.location.name,
+        id: ret.location.id,
+        imageSrc: ret.images.standard_resolution,
+        url: ret.link,
+        description: transformDescription(ret.caption.text)
+    };
 }
 
 schema.set('toJSON', {
-  transform: function(doc, ret, options) {
-    return {
-      created_time: ret.created_time,
-      coordinates: [ ret.location.longitude, ret.location.latitude ],
-      title: ret.location.name,
-      id: ret.location.id,
-      imageSrc: ret.images.standard_resolution,
-      url: ret.link,
-      description: transformDescription(ret.caption.text)
+    transform: function(doc, ret, options) {
+        return transformObject(ret);
     }
-  }
+});
+
+schema.set('toObject', {
+    transform: function(doc, ret, options) {
+        return transformObject(ret);
+    }
 });
 
 module.exports = {
-  Photo: Mongoose.model('Photo', schema)
+    Photo: Mongoose.model('Photo', schema)
 }
