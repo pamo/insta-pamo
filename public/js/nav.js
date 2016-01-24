c8334bd0b29fad9d84243c01d41d40853b724b1c
Pamela Ocampo <pamela.ocampo@gmail.com>
2016-01-23 22:26:47 -0800

    Adds aria hidden attribute to nav

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,6 +1,12 @@
 $(function() {
     $(".nav__trigger").on('click touchend', function(e) {
-        $(".nav").toggleClass("nav--open");
+        var navElement = $(".nav");
+        navElement.toggleClass("nav--open");
+        if(navElement.hasClass("nav--open")) {
+            navElement.removeAttr("aria-hidden");
+        } else {
+            navElement.attr("aria-hidden", true);
+        }
         e.preventDefault();
     });
 });
